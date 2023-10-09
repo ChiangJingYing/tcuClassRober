cd1beded0721cd4ac26b1158757b070ece015838
ewq897456321 <ltesr125124015@gmail.com>
2023-10-09 15:18:09 +0800

    fix windows webdriver .exe unsame with mac/linux

--- download_driver/download_webdrive.py
+++ download_driver/download_webdrive.py
@@ -16,19 +16,23 @@ def download_webdriver(chome_version: str):
     #   Get platfom string
     platform = Platform()
     if platform.platform == 'mac':
-        platform = f'{platform.platform}-{platform.machine}'
+        platform_full = f'{platform.platform}-{platform.machine}'
     else:
-        platform = platform.platform + platform.machine
+        platform_full = platform.platform + platform.machine
 
-    url = f'https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/{".".join(chome_version.split(".")[:3])}.0/{platform}/chromedriver-{platform}.zip'
-    
+    url = f'https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/{".".join(chome_version.split(".")[:3])}.0/{platform}/chromedriver-{platform_full}.zip'
+
     # Download file
     req = requests.get(url)
-    with open(resource_path(f'assets/chromeDriver/{platform}.zip'), 'wb') as f:
+    with open(resource_path(f'assets/chromeDriver/{platform_full}.zip'), 'wb') as f:
         f.write(req.content)
 
-    with ZipFile(resource_path(f'assets/chromeDriver/{platform}.zip')) as f:
-        f.extract(f'chromedriver-{platform}/chromedriver',
-                  resource_path(f'assets/chromeDriver'))
-        subprocess.call(['chmod', 'u+x', resource_path(f'assets/chromeDriver/chromedriver-{platform}/chromedriver')])
-    return resource_path(f'assets/chromeDriver/chromedriver-{platform}/chromedriver')
+    with ZipFile(resource_path(f'assets/chromeDriver/{platform_full}.zip')) as f:
+        if platform.platform == 'mac':
+            f.extract(f'chromedriver-{platform_full}/chromedriver',
+                    resource_path(f'assets/chromeDriver'))
+            subprocess.call(['chmod', 'u+x', resource_path(f'assets/chromeDriver/chromedriver-{platform_full}/chromedriver')])
+        elif platform.platform == 'win':
+            f.extract(f'chromedriver-{platform_full}/chromedriver.exe',
+                    resource_path(f'assets/chromeDriver'))
+    return resource_path(f'assets/chromeDriver/chromedriver-{platform_full}/chromedriver')
