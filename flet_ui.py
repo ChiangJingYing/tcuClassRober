d82bb0b92768870788e8201dd4a4e3738582cf04
JingChiang <ltesr125124015@gmail.com>
2023-10-11 20:44:45 +0800

    1. main page init by function 2. add route

--- flet_ui.py
+++ flet_ui.py
@@ -18,14 +18,9 @@ class Parameter():
         self.page_windows_height = 550
         self.countDelay = 10
 
-
-def main(page: ft.Page):
-    paramter = Parameter()
-
-    page.window_width = paramter.page_windows_width
-    page.window_height = paramter.page_windows_height
-
-    def rob():
+def show_main_page(paramter: Parameter):
+    def rob(**kwargs):
+        e = kwargs.get('e')
         count_code = list()
         for k in user_input_list:
             if k.value:
@@ -40,50 +35,51 @@ def main(page: ft.Page):
             print('Please input correct user info.')
             if paramter.running:
                 handle_stop_button_click('just put it not mean what')
-            page.snack_bar = ft.SnackBar(
+            e.page.snack_bar = ft.SnackBar(
                 content=ft.Text('請輸入正確的學號密碼及課程代碼！')
             )
-            page.snack_bar.open = True
-            page.update()
+            e.page.snack_bar.open = True
+            e.page.update()
 
-    def countDownTimes():
+    def countDownTimes(e):
         if count_down_text.value == '0':
-            rob()
+            rob(e)
             count_down_text.value = paramter.countDelay
         else:
             count_down_text.value = str(int(count_down_text.value) - 1)
-            page.update()
+            e.page.update()
 
-    def handle_start_button_click(_):
+    def handle_start_button_click(e):
         if user_input_delay_time.value == '':
-            page.snack_bar = ft.SnackBar(
+            e.page.snack_bar = ft.SnackBar(
                 content=ft.Text(value='請輸入正確的冷卻時間')
             )
-            page.snack_bar.open = True
-            page.update()
+            e.page.snack_bar.open = True
+            e.page.update()
 
         elif int(user_input_delay_time.value) <= 299:
-            page.snack_bar = ft.SnackBar(
+            e.page.snack_bar = ft.SnackBar(
                 content=ft.Text(value='冷卻時間需大於299')
             )
-            page.snack_bar.open = True
-            page.update()
+            e.page.snack_bar.open = True
+            e.page.update()
         elif paramter.timer is None or not paramter.running:
             paramter.countDelay = user_input_delay_time.value
             count_down_text.disabled = True
-            page.update()
-            paramter.timer = Timers(1, countDownTimes, )
+            e.page.update()
+            paramter.timer = Timers(1, countDownTimes,[],{'e': e})
             paramter.timer.start()
             paramter.running = True
             rob()
 
-    def handle_stop_button_click(_):
+    def handle_stop_button_click(e):
         count_down_text.value = str(paramter.countDelay)
         paramter.timer.cancel()
         paramter.running = False
         count_down_text.disabled = False
-        page.update()
+        e.page.update()
 
+    page = []
     user_input_student_number = ft.TextField(
         hint_text='113316187', content_padding=ft.padding.only(top=10, left=10),
         width=220, height=30, text_size=15,
@@ -119,36 +115,66 @@ def main(page: ft.Page):
     user_input_list = (user_input_code1, user_input_code2,
                        user_input_code3, user_input_code4)
 
-    page.add(ft.Row([
+    page.append(ft.Row([
         ft.Text(value='', width=200),
         count_down_text
     ]))
-    page.add(ft.Row([
+    page.append(ft.Row([
         ft.Text(value='學號：', width=50),
         user_input_student_number
     ]))
-    page.add(ft.Row([
+    page.append(ft.Row([
         ft.Text(value='密碼：', width=50),
         user_input_password
     ]))
-    page.add(ft.Divider())
-    page.add(ft.Text(value='課程代碼：', width=page.window_width - 30))
-    page.add(ft.Row([
+    page.append(ft.Divider())
+    page.append(ft.Text(value='課程代碼：', width=paramter.page_windows_width - 30))
+    page.append(ft.Row([
         user_input_code1,
         user_input_code2
     ]))
-    page.add(ft.Row([
+    page.append(ft.Row([
         user_input_code3,
         user_input_code4
     ]))
 
-    page.add(ft.Row([
+    page.append(ft.Row([
         ft.ElevatedButton(text='開始', on_click=handle_start_button_click,
-                          width=page.window_width / 2 - 50),
+                          width=paramter.page_windows_width / 2 - 50),
         ft.ElevatedButton(text='停止', on_click=handle_stop_button_click,
-                          width=page.window_width / 2 - 50)
+                          width=paramter.page_windows_width / 2 - 50)
     ]))
-    page.add(system_output)
+    page.append(system_output)
+    return page
+
+def main(page: ft.Page):
+    paramter = Parameter()
+
+    page.window_width = paramter.page_windows_width
+    page.window_height = paramter.page_windows_height
+
+    def on_route_change(route):
+        page.views.clear()
+        if page.route == '/':
+            page.views.append(
+                ft.View(
+                    '/',
+                    [control for control in show_main_page(paramter)]
+                )
+            )
+        elif page.route == '/setting':
+            page.views.append(
+                ft.View(
+                    '/setting',
+                    [ft.Text("Setting")]
+                )
+            )
+        
+        page.update()
+    
+    page.on_route_change = on_route_change
+    page.go('/')
+
 
 
 ft.app(target=main, assets_dir="assets")
